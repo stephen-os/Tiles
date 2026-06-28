d3d88731eb4d0854441882b3c3791dde79c7cf93
Stephen Watson <ImStephen.1sw@gmail.com>
2026-06-28 19:22:08 -0400

    refactor: make Layer methods pure virtual and add name
    - Add Layer constructor with name parameter
    - Make OnAttach, OnDetach, OnUpdate, OnRender, OnEvent pure virtual
    - Add m_name member and GetName() accessor

---
+++ Tiles/src/Tiles/Core/Layer.cpp
@@ -0,0 +1,6 @@
+#include "Layer.h"
+
+namespace Tiles
+{
+	Layer::Layer(const std::string& name) : m_name(name) {}
+}
--- Tiles/src/Tiles/Core/Layer.h
+++ Tiles/src/Tiles/Core/Layer.h
@@ -1,16 +1,23 @@
 #pragma once
 
+#include <string>
+
 namespace Tiles
 {
 	class Layer
 	{
 	public:
+		Layer(const std::string& name = "layer");
 		virtual ~Layer() = default;
 
-		virtual void OnAttach() {}
-		virtual void OnDetach() {}
+		virtual void OnAttach() = 0;
+		virtual void OnDetach() = 0;
+		virtual void OnUpdate() = 0;
+		virtual void OnRender() = 0;
+		virtual void OnEvent() = 0;
 
-		virtual void OnUpdate(float ts) {}
-		virtual void OnUIRender() {}
+		[[nodiscard]] const std::string& GetName() const { return m_name; }
+	private:
+		std::string m_name;
 	};
 }
