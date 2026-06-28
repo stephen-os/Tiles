9e7ba09d6671e6c47043edb335424c605e205a43
Stephen Watson <ImStephen.1sw@gmail.com>
2026-06-28 19:50:03 -0400

    fix: add timestep parameter to Layer::OnUpdate and rename OnRender to OnUIRender

--- Tiles/src/Tiles/Core/Layer.h
+++ Tiles/src/Tiles/Core/Layer.h
@@ -12,8 +12,8 @@ namespace Tiles
 
 		virtual void OnAttach() = 0;
 		virtual void OnDetach() = 0;
-		virtual void OnUpdate() = 0;
-		virtual void OnRender() = 0;
+		virtual void OnUpdate(float timestep) = 0;
+		virtual void OnUIRender() = 0;
 		virtual void OnEvent() = 0;
 
 		[[nodiscard]] const std::string& GetName() const { return m_name; }
